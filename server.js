178f297e73765ce054c87e2f291a29e0b4f7ea98
krishna chavali <krishna.chavali@vasudhaika.net>
2019-11-26 11:55:40 +0530

    learning git

--- server.js
+++ server.js
@@ -10,10 +10,17 @@ myEmitter.on('event', () => {
 myEmitter.emit('event');
 
 
-
+//Emitter
 const node1 = new Node1();
-
  node1.on('event', ()=> {
      console.log('first node from server')
  });
  node1.emit('event');
+
+
+//Timer
+ const node2 = () => {
+     console.log("helo after 4 seconds")
+ }
+ setTimeout(node2, 4*1000);
+
